b5cbfe6d35ac2125419306b8fa5be6c07e4f06da
Ikiwz <rifki.andriyana@gmail.com>
2024-12-04 18:49:20 +0700

    refactor(config): streamline application configuration setup
    Refactored the application configuration setup to improve readability and maintainability. Middleware configuration is now initialized as an empty array by default, and exception handling for API errors has been streamlined for better clarity.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,31 +1,24 @@
 <?php
 
+  use App\Helpers\ApiResponse;
   use Illuminate\Foundation\Application;
   use Illuminate\Http\Request;
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpKernel\Exception\HttpException;
 
-  return Application::configure(basePath: dirname(__DIR__))
+  return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-      web: __DIR__ . '/../routes/web.php',
-      api: __DIR__ . '/../routes/api.php',
-      commands: __DIR__ . '/../routes/console.php',
+      web: _DIR_ . '/../routes/web.php',
+      api: _DIR_ . '/../routes/api.php',
+      commands: _DIR_ . '/../routes/console.php',
       health: '/up',
     )
     ->withMiddleware(fn($middleware) => [])
-    ->withExceptions(fn($exceptions) => $exceptions->render(function (Throwable $e, Request $request) {
-      $statusCode = method_exists($e, 'getStatusCode')
-        ? $e->getStatusCode()
-        : Response::HTTP_INTERNAL_SERVER_ERROR;
-
-      $errorMessage = $e->getMessage() ?: 'An unexpected error occurred.';
-
-      return $request->is('api/*')
-        ? response()->json([
-          'success' => false,
-          'message' => $errorMessage,
-          'code' => $statusCode,
-          'trace' => env('APP_DEBUG') ? $e->getTrace() : null,
-        ], $statusCode)
-        : null;
-    }))
+    ->withExceptions(fn($exceptions) => $exceptions->render(fn(Exception $e, Request $request) => $request->is('api/*')
+      ? ApiResponse::error(
+        Response::$statusTexts[$statusCode = ($e instanceof HttpException) ? $e->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR] ?? 'An unexpected error occurred.',
+        $statusCode
+      )
+      : null
+    ))
     ->create();
